fa7e8a4a9cf0834109634c755475101351f4ee34
Duyen Thang <duyenthang.dev@gmail.com>
2022-05-27 13:10:04 +0700

    coding

--- components/Saved.js
+++ components/Saved.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
-// import 
+// import
+const img1 = require('./../assets/images/img1.jpg')
+const img2 = require('./../assets/images/img2.jpg')
+
+const SavedItem = () => {
+   return (
+      <View>
+         <Image source={img1} />
+         <View>
+           <Text>heeh</Text>
+           <Text></Text>
+         </View>
+      </View>
+   )
+}
+
 const Saved = () => {
-  return (
-    <View>
-      <Text>hhaaa</Text>
-    </View>
-  )
+   return (
+      <View>
+         <SavedItem />
+      </View>
+   )
 }
 
 export default Saved
